e39a19614413236c5b4470118e771d1ad07910a0
Konrad Śliwiński <konrad-sl@o2.pl>
2026-02-27 08:26:14 +0100

    Aktualizacja projektu

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-<<<<<<< HEAD
+// główne zakładki
+function openTab(tabName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+    // Ustawienie domyślnych podzakładek przy przejściu do odpowiedniej zakładki
+    if (tabName === "projects") {
+      openSubTab('autoenkoder');
+    }
+    if (tabName === "ongoing") {
+      openOngoingSubTab('ongoing1');
+    }
+  }
+  
+  // Funkcja do przełączania podzakładek w zakładce "projekty"
+  function openSubTab(subTabName) {
+    var i, subtab;
+    subtab = document.querySelectorAll("#projects .subtabcontent");
+    for (i = 0; i < subtab.length; i++) {
+      subtab[i].style.display = "none";
+    }
+    document.getElementById(subTabName).style.display = "block";
+  }
+  
+  // Funkcja do przełączania podzakładek w zakładce "projekty_w_trakcie"
+  function openOngoingSubTab(subTabName) {
+    var i, subtab;
+    subtab = document.querySelectorAll("#ongoing .subtabcontent");
+    for (i = 0; i < subtab.length; i++) {
+      subtab[i].style.display = "none";
+    }
+    document.getElementById(subTabName).style.display = "block";
+  }
+  
+  // Ustawienie domyślnej widoczności pierwszej zakładki
+  document.addEventListener("DOMContentLoaded", function() {
+    openTab('home');
+  });
 
 // główne zakładki
 function openTab(tabName) {
@@ -41,48 +81,3 @@ function openTab(tabName) {
   document.addEventListener("DOMContentLoaded", function() {
     openTab('home');
   });
-=======
-
-// główne zakładki
-function openTab(tabName) {
-    var i, tabcontent;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-    // Ustawienie domyślnych podzakładek przy przejściu do odpowiedniej zakładki
-    if (tabName === "projects") {
-      openSubTab('autoenkoder');
-    }
-    if (tabName === "ongoing") {
-      openOngoingSubTab('ongoing1');
-    }
-  }
-  
-  // Funkcja do przełączania podzakładek w zakładce "projekty"
-  function openSubTab(subTabName) {
-    var i, subtab;
-    subtab = document.querySelectorAll("#projects .subtabcontent");
-    for (i = 0; i < subtab.length; i++) {
-      subtab[i].style.display = "none";
-    }
-    document.getElementById(subTabName).style.display = "block";
-  }
-  
-  // Funkcja do przełączania podzakładek w zakładce "projekty_w_trakcie"
-  function openOngoingSubTab(subTabName) {
-    var i, subtab;
-    subtab = document.querySelectorAll("#ongoing .subtabcontent");
-    for (i = 0; i < subtab.length; i++) {
-      subtab[i].style.display = "none";
-    }
-    document.getElementById(subTabName).style.display = "block";
-  }
-  
-  // Ustawienie domyślnej widoczności pierwszej zakładki
-  document.addEventListener("DOMContentLoaded", function() {
-    openTab('home');
-  });
->>>>>>> 8ecd8d059617da6cd72ec7eb62d67f253ae5dd92
-  
